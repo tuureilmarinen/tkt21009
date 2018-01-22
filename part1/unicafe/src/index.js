e6cca44d52cea6d54dd1a6e10172e7d9580630f7
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2018-01-22 19:30:49 +0200

    completed tasks 1.7-1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const Display= ({text,count}) => {
+const Statistic= ({text,count}) => {
   return (
     <div><b>{text}:</b> {count}</div>
   )
@@ -13,7 +13,7 @@ const Button = ({handleClick,text}) => {
     {text}
   </button>)
 }
-class Counter extends React.Component {
+class Statistics extends React.Component {
   constructor() {
     super()
     this.state = {
@@ -22,16 +22,26 @@ class Counter extends React.Component {
       bad: 0
     }
   }
-    good = () => {
-      this.setState({good: this.state.good+1})
-    }
-    bad = () => {
-      this.setState({bad: this.state.bad+1})
-    }
-    neutral = () => {
-      this.setState({neutral: this.state.neutral+1})
-    }
-
+  good = () => {
+    this.setState({good: this.state.good+1})
+  }
+  bad = () => {
+    this.setState({bad: this.state.bad+1})
+  }
+  neutral = () => {
+    this.setState({neutral: this.state.neutral+1})
+  }
+  average = () => {
+    /*return () => {
+      return 609
+    }*/
+    return (
+      ((this.state.good-this.state.bad)/(this.state.good+this.state.bad+this.state.neutral))||0
+    )
+  }
+  positive_percentage = () => {
+    return ((((this.state.good)/(this.state.good+this.state.bad+this.state.neutral))*100)||0)+"%"
+  }
   render(){
     return (
       <div>
@@ -40,9 +50,11 @@ class Counter extends React.Component {
         <Button handleClick={this.neutral} text="Neutraali"/>
         <Button handleClick={this.bad} text="Huono"/>
         <h1>Statistiikka</h1>
-        <Display text="hyvä" count={this.state.good} />
-        <Display text="neutraali" count={this.state.neutral} />
-        <Display text="huono" count={this.state.bad} />
+        <Statistic text="hyvä" count={this.state.good} />
+        <Statistic text="neutraali" count={this.state.neutral} />
+        <Statistic text="huono" count={this.state.bad} />
+        <Statistic text="keskiarvo" count={this.average()} />
+        <Statistic text="positiivisia" count={this.positive_percentage()} />
       </div>
     )
   }
@@ -50,7 +62,7 @@ class Counter extends React.Component {
 
 const App = () => {
   return (
-    <Counter />
+    <Statistics />
   )
 }
 
