1796b62d64a4485e93210cc95ddbbe25a33d48a8
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2018-01-24 22:28:54 +0200

    completed tasks 1.9-1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 const Statistic= ({text,count}) => {
   return (
-    <div><b>{text}:</b> {count}</div>
+    <tr><td><b>{text}:</b></td><td>{count}</td></tr>
   )
 }
 const Button = ({handleClick,text}) => {
@@ -21,20 +21,21 @@ class Statistics extends React.Component {
       neutral: 0,
       bad: 0
     }
+    //this.vote=this.vote.bind(this)
   }
-  good = () => {
-    this.setState({good: this.state.good+1})
-  }
-  bad = () => {
-    this.setState({bad: this.state.bad+1})
-  }
-  neutral = () => {
-    this.setState({neutral: this.state.neutral+1})
+  vote = (arg) => {
+    let temp =() => {
+      let val=this.state[arg]+1;
+      let oldState=this.state;
+      oldState[arg]=val;
+      this.setState(oldState)
+      //debugger
+    }
+    temp=temp.bind(this)
+    return temp
   }
   average = () => {
-    /*return () => {
-      return 609
-    }*/
+    console.log(this)
     return (
       ((this.state.good-this.state.bad)/(this.state.good+this.state.bad+this.state.neutral))||0
     )
@@ -43,20 +44,36 @@ class Statistics extends React.Component {
     return ((((this.state.good)/(this.state.good+this.state.bad+this.state.neutral))*100)||0)+"%"
   }
   render(){
-    return (
-      <div>
-        <h1>Anna palautetta</h1>
-        <Button handleClick={this.good} text="Hyvä"/>
-        <Button handleClick={this.neutral} text="Neutraali"/>
-        <Button handleClick={this.bad} text="Huono"/>
-        <h1>Statistiikka</h1>
-        <Statistic text="hyvä" count={this.state.good} />
-        <Statistic text="neutraali" count={this.state.neutral} />
-        <Statistic text="huono" count={this.state.bad} />
-        <Statistic text="keskiarvo" count={this.average()} />
-        <Statistic text="positiivisia" count={this.positive_percentage()} />
-      </div>
-    )
+    if(this.state.good+this.state.neutral+this.state.bad>0){
+      return (
+        <div>
+          <h1>Anna palautetta</h1>
+          <Button handleClick={this.vote('good').bind(this)} text="Hyvä"/>
+          <Button handleClick={this.vote('neutral')} text="Neutraali"/>
+          <Button handleClick={this.vote('bad')} text="Huono"/>
+          <h1>Statistiikka</h1>
+          <table>
+            <tbody>
+              <Statistic text="hyvä" count={this.state.good} />
+              <Statistic text="neutraali" count={this.state.neutral} />
+              <Statistic text="huono" count={this.state.bad} />
+              <Statistic text="keskiarvo" count={this.average()} />
+              <Statistic text="positiivisia" count={this.positive_percentage()} />
+            </tbody>
+          </table>
+        </div>
+      )} else {
+        return (
+          <div>
+            <h1>Anna palautetta</h1>
+            <Button handleClick={this.vote('good')} text="Hyvä"/>
+            <Button handleClick={this.vote('neutral')} text="Neutraali"/>
+            <Button handleClick={this.vote('bad')} text="Huono"/>
+            <h1>Statistiikka</h1>
+            <p>Ei arvosteluja.</p>
+          </div>
+        )
+    }
   }
 }
 
